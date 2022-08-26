3fe90c39f904181efd5d1d4dbaeecc4ca458c241
farman <ahmadfarman1701@gmail.com>
2022-08-26 12:46:31 +0200

    editing k3s

--- k3s.sh
+++ k3s.sh
@@ -24,16 +24,16 @@ curl -sfL https://get.k3s.io | sh -s -
 #curl -sfL https://get.k3s.io | K3S_URL=https://${CONTROLLER_SERVER_IP}:6443 sh
 
 #INSTALL DOCKER :
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
-sudo apt update
+apt update
 apt-cache policy docker-ce
 
-sudo apt install docker-ce
-sudo usermod -aG docker ${USER}
+apt install docker-ce
+usermod -aG docker ${USER}
 su - ${USER}
 id -nG
-sudo usermod -aG docker username
+usermod -aG docker username
